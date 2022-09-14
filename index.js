724a1fbbf6b93c0db79f2f033ef291a46a65439c
vaMoreth <107073270+vaMoreth@users.noreply.github.com>
2022-09-14 23:12:22 +0200

    Add full version drumkit site

---
+++ index.js
@@ -0,0 +1,48 @@
+let numOfDrumBtn = document.querySelectorAll(".drum").length;
+
+for(let i = 0; i < numOfDrumBtn; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        
+        let btnInnerHTML = this.innerHTML;
+        makeSound(btnInnerHTML);
+    });
+}
+
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+});
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            let kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+        default: console.log(btnInnerHTML);
+    }
+}
